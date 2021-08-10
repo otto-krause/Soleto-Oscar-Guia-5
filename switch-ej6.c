9d74cfb187c660578d3a20f73852a7f7bf35f0e3
ashandme <santiagosoleto121@gmail.com>
2021-08-10 14:03:59 -0300

    added

---
+++ switch-ej6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+  int dia, mes;
+  scanf("%d %d", &dia, mes);
+  switch(mes){
+    case 1:
+      if(dia<21)
+        printf("Capricornio");
+      else
+        printf("Acuario");
+      break;
+    case 2:
+   
+  }
+}
